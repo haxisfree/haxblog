c9755764bbeca0c1b1159632b0d1626faa4351cb
Amirhossein <amirhossein47khalil@yahoo.com>
2021-11-25 13:05:59 +0330

    admin panel changed and some fields added

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-
 from .models import Post
 
-admin.site.register(Post)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pubdate', 'category', 'status')
+    list_filter = ('category', 'status')
+    search_fields = ('title', 'body')
+    ordering = ['status', 'pubdate']
+
+
+admin.site.register(Post, BookAdmin)
