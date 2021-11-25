3a35d6bae99114d88720cdfbbae495cd5c08288e
Amirhossein <amirhossein47khalil@yahoo.com>
2021-11-25 12:01:01 +0330

    Post model updated

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,33 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(
-            'auth.User',
-            on_delete=models.CASCADE,
+    STATUS_CHOICES = (
+        ('d' , 'Draft') , 
+        ('p' , 'Published')
     )
-
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100, unique=True)
     body = models.TextField()
+    #image = models.imagefields()
+    pubdate = models.DateTimeField(default = timezone.now)
+    year = models.IntegerField ()
+    author = models.CharField(max_length=100)
+    pagenum = models.IntegerField ()
+    category = models.CharField(max_length=100)
+    status = models.CharField(max_length=1, choices = STATUS_CHOICES)
+
+
+    # author = models.ForeignKey(
+    #         'auth.User',
+    #         on_delete=models.CASCADE,
+    # )
+
+
+
+   
 
     def __str__(self):
         return self.title
