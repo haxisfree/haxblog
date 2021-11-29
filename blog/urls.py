6732bbdd722142ac23c7a1ea06d77728b11f9ce3
Amirhossein <amirhossein47khalil@yahoo.com>
2021-11-29 13:53:10 +0330

    pagination is implemented

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 #blog/urls.py
 
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogDetailView, home_page
 
+# app_name = "Blog"
 urlpatterns = [
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
-    path('', BlogListView.as_view(), name='home'),
+    path('', home_page , name='home'),
+    path('page/<int:page>', home_page , name='home'),
+    # path('page/<int:page>', BlogListView.as_view(), name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from .models import Post
+from django.core.paginator import Paginator
+# from . import models
 # Create your views here.
 
-class BlogListView(ListView):
-    model = Post
-    template_name = 'home.html'
+def home_page(request, page=1):
+    # context_object_name='posts_list'
+    # model = Post
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 3)
+    page = request.GET.get('page')
+    page_obj = paginator.get_page(page)
+    context = {
+        "post" : page_obj,
+        }
+    return render (request, 'home.html', context)
+
+
+
+# class BlogListView(ListView):
+    # queryset  = Post.objects.all()
+    # context_object_name='posts_list'
+    # paginate_by = 3
+    # model = Post
+    # template_name = 'home.html'
+    
+    # def get_queryset(self):
+    #     """Return Schools """
+    #     return models.Post.objects.order_by('id')
+    
 
 class BlogDetailView(DetailView):
     model = Post
